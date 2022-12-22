64321486ee0ca7439a3834dd7f2e8a5fc441931d
Dynylson <dynylsonjr@gmail.com>
2022-12-22 11:19:41 -0300

    BannerHome Responsivo

--- src/components/BannerHome/index.tsx
+++ src/components/BannerHome/index.tsx
@@ -7,21 +7,25 @@ export function BannerHome() {
   });
 
   return (
-    <Flex
-      bgImage={isWideVersion ? 'url("/assets/bannerHome.png")' : ""}
-      bgPosition='center'
-      height='650px'
-      maxW='100%'
-    >
-      <Flex direction='column' maxW={1700} mx='auto' w='100%' mt='3.9rem'>
-        <Heading color='white.900' fontSize='3rem'>
-          Não sabe o que assistir no fim de semana?
-        </Heading>
-        <Text color='white.900' fontSize='2.25rem' mt='5rem'>
-          Venha dar uma olhada nos filmes em alta, confira seus trailers e{" "}
-          <br /> monte sua própria lista de filmes!
-        </Text>
-      </Flex>
-    </Flex>
+    <>
+      {isWideVersion && (
+        <Flex
+          bgImage='url("/assets/bannerHome.png")'
+          bgPosition='center'
+          height='650px'
+          maxW='100%'
+        >
+          <Flex direction='column' maxW={1700} mx='auto' w='100%' mt='3.9rem'>
+            <Heading color='white.900' fontSize='3rem'>
+              Não sabe o que assistir no fim de semana?
+            </Heading>
+            <Text color='white.900' fontSize='2.25rem' mt='5rem'>
+              Venha dar uma olhada nos filmes em alta, confira seus trailers e{" "}
+              <br /> monte sua própria lista de filmes!
+            </Text>
+          </Flex>
+        </Flex>
+      )}
+    </>
   );
 }
