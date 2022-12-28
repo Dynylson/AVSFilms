54eb3ba582b5ace96276fa5e1e7e21ea43de2f09
Dynylson <dynylsonjr@gmail.com>
2022-12-27 21:07:22 -0300

    Adicionado nota aos filmes

--- src/components/Movies/MovieCard/index.tsx
+++ src/components/Movies/MovieCard/index.tsx
@@ -1,22 +1,53 @@
-import { Flex, Image, Heading } from "@chakra-ui/react";
+import { Flex, Image, Heading, Box, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import { AiFillStar } from "react-icons/ai";
 
 interface MovieCardProps {
+  id: number;
   src: string;
   alt: string;
   title: string;
+  vote_average: number;
 }
 
-export function MovieCard({ src, alt, title }: MovieCardProps) {
+export function MovieCard({
+  id,
+  src,
+  alt,
+  title,
+  vote_average,
+}: MovieCardProps) {
   const getPosterURL = (posterpath: string) => {
     return `https://www.themoviedb.org/t/p/w220_and_h330_face${posterpath}`;
   };
 
   return (
-    <Flex direction='column' alignItems='start'>
-      <Image src={getPosterURL(src)} alt={alt} borderRadius='7px' />
-      <Heading color='gray.300' fontSize={[".9rem", "1.5rem"]} mt='.8rem'>
-        {title}
-      </Heading>
-    </Flex>
+    <Link href={`/movies/${id}`}>
+      <Flex direction='column' alignItems='start'>
+        <Box position='relative'>
+          <Image
+            src={getPosterURL(src)}
+            alt={alt}
+            borderRadius='7px'
+            width='300px'
+          />
+          <Flex
+            position='absolute'
+            bottom='5'
+            left='4'
+            alignItems='center'
+            gap='.3rem'
+          >
+            <AiFillStar size={25} fill='#F3F808' />
+            <Text color='blue.900' fontWeight='bold'>
+              {vote_average}
+            </Text>
+          </Flex>
+        </Box>
+        <Heading color='gray.300' fontSize={[".9rem", "1.5rem"]} mt='.8rem'>
+          {title}
+        </Heading>
+      </Flex>
+    </Link>
   );
 }
