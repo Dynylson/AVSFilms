c2cc428bd45296066ede2f03f593f3f445990f15
Dynylson <dynylsonjr@gmail.com>
2023-01-05 18:58:51 -0300

    Componente MovieSimilar responsivo

--- src/components/Movies/MoviesList/MovieDetails/MovieSimilar/index.tsx
+++ src/components/Movies/MoviesList/MovieDetails/MovieSimilar/index.tsx
@@ -1,4 +1,4 @@
-import { Flex, Heading, Image } from "@chakra-ui/react";
+import { Flex, Heading, Image, useBreakpointValue } from "@chakra-ui/react";
 import { IMovie } from "../../../../../../typings";
 import { Splide, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css";
@@ -11,33 +11,42 @@ interface MovieSimilarProps {
 }
 
 export function MovieSimilar({ similar }: MovieSimilarProps) {
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
   return (
-    <Flex ml='15rem'>
-      <Flex direction='column'>
-        <Heading fontSize='2rem'>Filmes Similares</Heading>
-        <Flex direction='column'>
-          <Splide options={{ width: 600, perPage: 3, gap: "1rem" }}>
-            {similar.map(({ id, poster_path, name }) => {
-              return (
-                <SplideSlide key={id}>
-                  <Link href={`/movie/${id}`}>
-                    <Flex maxW='100%' w='210px'>
-                      <Image
-                        src={getMovieImage(poster_path)}
-                        alt={name}
-                        borderRadius='8px'
-                        bg='blue'
-                        maxW='100%'
-                      />
-                      <Heading>{name}</Heading>
-                    </Flex>
-                  </Link>
-                </SplideSlide>
-              );
-            })}
-          </Splide>
+    <>
+      {isWideVersion && (
+        <Flex ml='15rem'>
+          <Flex direction='column'>
+            <Heading fontSize='2rem'>Filmes Similares</Heading>
+            <Flex direction='column'>
+              <Splide options={{ width: 600, perPage: 3, gap: "1rem" }}>
+                {similar.map(({ id, poster_path, name }) => {
+                  return (
+                    <SplideSlide key={id}>
+                      <Link href={`/movie/${id}`}>
+                        <Flex maxW='100%' w='210px'>
+                          <Image
+                            src={getMovieImage(poster_path)}
+                            alt={name}
+                            borderRadius='8px'
+                            bg='blue'
+                            maxW='100%'
+                          />
+                          <Heading>{name}</Heading>
+                        </Flex>
+                      </Link>
+                    </SplideSlide>
+                  );
+                })}
+              </Splide>
+            </Flex>
+          </Flex>
         </Flex>
-      </Flex>
-    </Flex>
+      )}
+    </>
   );
 }
