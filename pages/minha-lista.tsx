7019439fd14577ca557445818f397307955a4349
Dynylson <dynylsonjr@gmail.com>
2023-03-12 15:03:26 -0300

    minha-lista adapted to mobile version

--- pages/minha-lista.tsx
+++ pages/minha-lista.tsx
@@ -1,4 +1,12 @@
-import { Button, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Flex,
+  Heading,
+  Image,
+  Text,
+  useBreakpointValue,
+} from "@chakra-ui/react";
 import { RiH1 } from "react-icons/ri";
 import { useAddMoviesList } from "../src/hooks/useAddMoviesList";
 import { getMovieImage } from "../src/utils/requests";
@@ -9,6 +17,11 @@ export default function MinhaLista() {
   // function handleRemoveMovieFromList() {
   //   const movieExists = moviesList.find((movie) => movie.title === title)
   // }
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
 
   return (
     <Flex
@@ -36,16 +49,28 @@ export default function MinhaLista() {
                 <Image
                   src={getMovieImage(movie.poster_path)}
                   alt={movie.title}
-                  w='150px'
+                  maxW='140px'
                   borderRadius='8px'
                 />
-                <Flex flexDirection='column'>
-                  <Heading as='h2' fontSize='1.5rem'>
-                    {movie.title}
-                  </Heading>
-                  <Text maxW='70ch'>{movie.overview}</Text>
+                <Flex
+                  flexDirection='column'
+                  justifyContent={["stretch", "space-between"]}
+                  alignItems='start'
+                  gap={[".5rem", "0rem"]}
+                >
+                  <Box>
+                    <Heading as='h2' fontSize={["1.1rem", "1.5rem"]}>
+                      {movie.title}
+                    </Heading>
+                    {isWideVersion && movie.overview ? (
+                      <Text maxW='70ch' mt='.5rem'>
+                        {movie.overview}
+                      </Text>
+                    ) : (
+                      <Text>(Não possui sinopse ainda 😒)</Text>
+                    )}
+                  </Box>
                   <Button
-                    alignSelf='end'
                     background='#c53030'
                     color='white'
                     _hover={{ background: "#9B2C2C" }}
