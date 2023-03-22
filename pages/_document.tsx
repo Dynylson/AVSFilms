fac0b1482f228700eac41714b6b4b250c0744654
Dynylson <dynylsonjr@gmail.com>
2023-03-21 23:43:30 -0300

    Favicon and title added

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,30 @@ import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang='en'>
+    <Html lang='pt-BR'>
+      <title>AVSFilms</title>
+
+      <link
+        rel='apple-touch-icon'
+        sizes='180x180'
+        href='/apple-touch-icon.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='32x32'
+        href='/favicon-32x32.png'
+      />
+      <link
+        rel='icon'
+        type='image/png'
+        sizes='16x16'
+        href='/favicon-16x16.png'
+      />
+      <link rel='manifest' href='/site.webmanifest' />
+
+      <link rel='icon' type='image/x-icon' href='/assets/favicon.ico' />
+
       <Head>
         <link rel='preconnect' href='https://fonts.googleapis.com' />
         <link rel='preconnect' href='https://fonts.gstatic.com' />
--- src/components/TvShowActors/index.tsx
+++ src/components/TvShowActors/index.tsx
@@ -4,14 +4,14 @@ import { TvShowActorsCard } from "./TvShowActorsCard";
 import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/mousewheel";
-import { Heading } from "@chakra-ui/react";
+import { Button } from "@chakra-ui/react";
 
 interface TvShowActorsProps {
   actors: ITvShowActors[];
 }
 
 export function TvShowActors({ actors }: TvShowActorsProps) {
-  const arrayReduced = actors.filter((actor, index) => {
+  const arrayReduced = actors?.filter((actor, index) => {
     return index < 20;
   });
 
@@ -29,25 +29,4 @@ export function TvShowActors({ actors }: TvShowActorsProps) {
       </Swiper>
     </>
   );
-
-  // return (
-  //   // <Splide
-  //   //   options={{
-  //   //     perPage: 9,
-  //   //     perMove: 3,
-  //   //     drag: "free",
-  //   //     gap: ".3rem",
-  //   //     arrows: false,
-  //   //     pagination: false,
-  //   //   }}
-  //   // >
-  //     {actors?.map((actor) => {
-  //       return (
-  //         // <SplideSlide key={actor.id}>
-  //           <TvShowActorsCard data={actor} />
-  //         // </SplideSlide>
-  //       );
-  //     })}
-  //   // </Splide>
-  // );
 }
